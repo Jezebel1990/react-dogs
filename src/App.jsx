8cada7024043b7273ca41b2acab851ec27223327
Jezebel1990 <jezebelguedes@gmail.com>
2023-08-22 18:15:46 -0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import './App.css';
-import Api from "./api/Api";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+
 
 function App() {
 
   return (
   <div>
-    <Api />
+    <BrowserRouter>
+    <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+    <Footer />
+    </BrowserRouter>
     </div>
   );     
 };
