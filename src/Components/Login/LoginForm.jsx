60df10949287bc627e3e72016de0d0f9f9f8aadf
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-13 18:24:32 -0300

    feat: add useForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,19 +2,24 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const username = useForm();
+    const password = useForm();
+
 
     function handleSubmit(event) {
         event.preventDefault();
+
+        if(username.validate() && password.validate()) {
+
         fetch('https://dev-dogsapi.pantheonsite.io/json/jwt-auth/v1/token/auth', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify({username, password}),
+            body: JSON.stringify(),
         })
          .then((response) => {
             console.log(response);
@@ -23,14 +28,15 @@ const LoginForm = () => {
          .then((json) => {
             console.log(json);
          });
+        }
     }
 
     return (
     <section>
     <h1>Login</h1>
     <form action="" onSubmit={handleSubmit}>
-     <Input label="Usuário" type="text" name="username"/>   
-     <Input label="Senha" type="password" name="password" />
+     <Input label="Usuário" type="text" name="username" {...username}/>   
+     <Input label="Senha" type="password" name="password" {...password} />
     <Button>Entrar</Button>
     </form>
   <Link to="/login/criar"> Cadastros</Link>
