4e1008cf84deeb842bf2b01e8128672797eb0e36
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-20 23:40:49 -0300

    feat: add user component

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import UserHeader from './UserHeader';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import { Routes, Route } from 'react-router-dom';
 
 const User = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+  <section className="container">
+    <UserHeader />
+    <Routes>
+      <Route  path="/" element={<Feed />}/>
+      <Route path="/post"  element={<UserPhotoPost />}/>
+      <Route path="/statistic" element={<UserStats />} />
+    </Routes>
+  </section>
+  );
+};
 
 export default User
