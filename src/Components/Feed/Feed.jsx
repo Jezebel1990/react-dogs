7ad18aae0de43411bba69725993cbff4baccab47
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-03 22:51:16 -0300

    feat: add feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
-  return (
+  return 
     <div>
-      
+     <FeedModal/>
+     <FeedPhotos />
     </div>
-  )
+  
 }
 
 export default Feed;
---
+++ src/Components/Feed/FeedPhotos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FeedPhotosItem from './FeedPhotosItem';
+
+const FeedPhotos = () => {
+  return (
+    <div>
+      <FeedPhotosItem />
+    </div>
+  )
+}
+
+export default FeedPhotos;
