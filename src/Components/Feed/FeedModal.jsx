b64ece1892b9ae9085a078acb0e6749e1169e272
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-12 21:59:45 -0300

    feat: add feed modal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import { PHOTO_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = () => {
+
+const FeedModal = ({photo}) => {
+const {data, error, loading, request } = useFetch();
+
+React.useEffect(() => {
+  const { url, options } = PHOTO_GET(photo.id);
+  request(url, options);
+}, [photo, request]);
+
   return (
-    <div>
-      
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent  data={data} />}
     </div>
-  )
+  );
 }
 
 export default FeedModal;
---
+++ src/Components/Photo/PhotoContent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
+
+const PhotoContent = ({ data }) => {
+    const {photo, comments} = data;
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}><img src={photo.src} alt={photo.title} /></div>
+      <div className={styles.details}>
+      <div>
+        <p>
+         <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>   
+         <span className={styles.views}>{photo.acessos}</span>
+        </p>
+        <h1 className="title">
+            <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
+        </h1>
+        <ul className={styles.attributes}>
+        <li>{photo.peso} Kg</li>
+        <li>{photo.idade} anos</li>
+        </ul>
+      </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments}/>
+    </div>
+  )
+}
+
+export default PhotoContent
