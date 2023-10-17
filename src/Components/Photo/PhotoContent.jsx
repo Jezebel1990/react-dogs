703e817d369a85587f08fcd969cf3867d2cfc596
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-16 23:02:27 -0300

    feat: add  photo delete

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
-    const {photo, comments} = data;
+   const user = React.useContext(UserContext);
+    const { photo, comments } = data;
   return (
     <div className={styles.photo}>
       <div className={styles.img}><img src={photo.src} alt={photo.title} /></div>
       <div className={styles.details}>
       <div>
         <p className={styles.author}>
-         <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>   
+          {user.data && user.data.username === photo.author ? ( 
+          <PhotoDelete id={photo.id} />
+          ) : (
+         <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+          )}   
+          
          <span className={styles.views}>{photo.acessos}</span>
         </p>
         <h1 className="title">
