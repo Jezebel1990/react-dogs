fa94985c061f839bc1d3e05b1bad358c6393ba81
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-19 23:54:06 -0300

    feat: add scroll infinito

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,13 +4,16 @@ import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import { Routes, Route } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const {data} = React.useContext(UserContext);
+
   return (
   <section className="container">
     <UserHeader />
     <Routes>
-      <Route  path="/" element={<Feed />}/>
+      <Route  path="/" element={<Feed user={data.id} />}/>
       <Route path="/post"  element={<UserPhotoPost />}/>
       <Route path="/statistic" element={<UserStats />} />
     </Routes>
