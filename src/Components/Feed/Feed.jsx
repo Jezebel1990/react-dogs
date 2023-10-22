9f005b3937d16056af22df62568a27a784cfa480
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-22 12:59:59 -0300

    feat: add prop types

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
+import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -44,8 +45,30 @@ React.useEffect(() => {
       setInfinite={setInfinite}
       />
       ))}
+      {!infinite && !user && (
+        <p
+        style={{
+          textAlign: 'center',
+          padding: '2rem 0 4rem 0',
+          color: '#888',
+        }}
+        >
+          Não existem mais postagens.
+        </p>
+      )}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+     PropTypes.string.isRequired,
+     PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
