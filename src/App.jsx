a5ba6767decf9a2afb11439f1cf46b22fd79fd9f
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-25 22:45:07 -0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,11 @@ import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-  <div>
+  <div className="App">
     <BrowserRouter>
     <UserStorage>
     <Header />
+    <main className="AppBody">
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="login/*" element={<Login />} />
@@ -32,6 +33,7 @@ function App() {
     <Route path="profile/:user" element={<UserProfile />}/>
     <Route path="*" element={<NotFound />} />
   </Routes>
+  </main>
     <Footer />
     </UserStorage>
     </BrowserRouter>
