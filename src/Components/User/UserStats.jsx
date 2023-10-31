94dd7952b458d03259f6e6664c5ad4f06b009f8c
Jezebel1990 <jezebelguedes@gmail.com>
2023-10-31 01:09:25 -0300

    feat: add statistics

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import Head from '../Helper/Head'
+import useFetch from '../../Hooks/useFetch'
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
 
 const UserStats = () => {
+const {data, error, loading, request} = useFetch();
+
+React.useEffect(() => {
+  async function getData() {
+  const { url, options } = STATS_GET();
+  await request(url, options)
+}
+getData();
+}, [request]);
+
+
+if(loading) return <Loading />
+if(error) return <Error error={error} />
   return (
     <div>
       <Head title="Estatísticas"/>
--- src/api.jsx
+++ src/api.jsx
@@ -145,3 +145,15 @@ export function PHOTO_POST(formData, token) {
       },
     };
   }
+
+export function STATS_GET() {
+  return {
+    url: API_URL + '/api/stats',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      }
+    }
+  }
+}
